494d5169e13f5e64f2796b5293a7a9e4e2756e72
Muhammadamin <webhacker0100@gmail.com>
2022-02-27 13:59:50 +0500

    Added template.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>Hello world!</h1>
+    <div class="app__wrap">
+        <main>
+            <section class="location">
+                <div class="city">Tashkent, UZ</div>
+                <div class="date">Monday, 22 September 2021</div>
+            </section>
+            <div class="main__temp">
+                <div class="temp">8<span>°c</span></div>
+                <div class="weather">Fog</div>
+                <div class="hi__low">19°c / 24°c</div>
+            </div>
+        </main>
+        <header>
+            <div class="content"><input type="text" autocomplete="off" class="search__box" placeholder="Search...">
+            </div>
+        </header>
+    </div>
 </template>
 
 <script>
