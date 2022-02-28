ff7a4cd5ffef9d88114078ed024a335709475e92
Muhammadamin <webhacker0100@gmail.com>
2022-02-28 21:17:52 +0500

    Import 'main.css' file.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
+import '../src/assets/main.css'
 
 createApp(App).use(store).mount('#app')
