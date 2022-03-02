342c64aba0aecc6c47015b6bd47c829a56ff12a6
Muhammadamin <webhacker0100@gmail.com>
2022-03-02 21:33:13 +0500

    Building for production.

---
+++ dist/precache-manifest.f0b9d3adf055bb90db98b76ac9fb030a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b98f66b87582b3f722",
+    "url": "/css/app.77836c85.css"
+  },
+  {
+    "revision": "62d1cb469a4bc39209742922d864ec20",
+    "url": "/img/bg.62d1cb46.jpg"
+  },
+  {
+    "revision": "ec8240cf6475fdc5ac9b1671c135dcd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75b98f66b87582b3f722",
+    "url": "/js/app.df67f57f.js"
+  },
+  {
+    "revision": "a945e3b3e2f610a240ce",
+    "url": "/js/chunk-vendors.2d38e111.js"
+  },
+  {
+    "revision": "04f03e59d9481f48cf5770bbfe7e1f94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
